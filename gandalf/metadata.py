a3c3e66b68162304ae314680a4bedb2337634496
Allan Swanepoel <allanice001@gmail.com>
2019-08-11 22:50:24 +0100

    Bump version: 0.2.0 → 0.2.1

--- gandalf/metadata.py
+++ gandalf/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = "gandalf"
 project = "Providing Blazingly fast & beautifully expressive Web Apps and APIs"
 project_no_spaces = project.replace(" ", "")
-version = "0.2.0"
+version = "0.2.1"
 description = (
     "Simple and rapid application development framework, built on top of Flask."
     " includes detailed security, auto CRUD generation for your models,"
