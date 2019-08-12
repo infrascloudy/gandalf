1a018032056b70bd4b5c9836a0cce1b7997b9381
Allan Swanepoel <allanice001@gmail.com>
2019-08-12 01:00:24 +0100

    Removing SQLALCHEMY_DATABASE_URI Test

--- gandalf/tests/test_config.py
+++ gandalf/tests/test_config.py
@@ -18,9 +18,6 @@ class TestDevelopmentConfig(TestCase):
     def test_app_is_development(self):
         self.assertTrue(app.config["SECRET_KEY"] == "my_precious")
         self.assertFalse(current_app is None)
-        self.assertTrue(
-            app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL")
-        )
 
 
 class TestTestingConfig(TestCase):
@@ -32,9 +29,6 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config["SECRET_KEY"] == "my_precious")
         self.assertTrue(app.config["TESTING"])
         self.assertFalse(app.config["PRESERVE_CONTEXT_ON_EXCEPTION"])
-        self.assertTrue(
-            app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_TEST_URL")
-        )
 
 
 class TestProductionConfig(TestCase):
